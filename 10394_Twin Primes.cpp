f952b53e81e4aab4a0310ea60716cfdbf771db9f
StarCoral <cabbapig1229@gmail.com>
2018-12-17 20:09:38 +0800

    Verdict: Accepted
    Verdict: Accepted

--- 10394_Twin Primes.cpp
+++ 10394_Twin Primes.cpp
@@ -5,54 +5,39 @@
 				2018/12/16 StarCoral
 **/
 #include<iostream>
-#include <string.h>
 #include<vector>
-#define SIZE 20000001
-
+#define SIZE 20000000
 using namespace std;
-
-struct twin
-{
-	int x,y;
-	
-	twin(){}
-	twin(int xx,int yy)
-		:x(xx),y(yy)
-	{
-	}
-};
 
 int main()
 {
-	bool prime[SIZE];
-	memset(prime,true,sizeof(prime));
+	vector<bool> prime(SIZE,true);
+	vector<int> twinPrime;
+	
 	prime[0] = prime[1]=false;
 	
 	int i,j;
 	
-	for(i = 2 ; i < SIZE ; i++)
+	for(i = 2 ; i <= SIZE ; i++)
 	{
 		if(prime[i])
 		{
-			for(j = i*i ; j < SIZE ; j+=i)
+			for(j = i+i ; j < SIZE ; j+=i)
 			{
-				prime[j] = 0;
+				prime[j] = false;
 			}
 		}
-	}
-	
-	vector<twin>twinPrime;
-	for(i = 3 ; i < SIZE-2 ; i++)
-	{
-		if(prime[i] && prime[i+2])
-			twinPrime.push_back(twin(i,i+2));
+		if(prime[i] && prime[i-2])
+		{
+			twinPrime.push_back(i-2);
+		}
 	}
 	
 	int pair;
 	
 	while(cin>>pair)
 	{
-		cout<<"("<<twinPrime[pair-1].x<<", "<<twinPrime[pair-1].y<<")\n";
+		cout<<"("<<twinPrime[pair-1]<<", "<<twinPrime[pair-1]+2<<")\n";
 	}
 	
 	return 0;
